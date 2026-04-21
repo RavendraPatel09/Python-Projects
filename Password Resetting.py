38c101bafc605631ff788779b14f18943edd3943
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-22 00:00:27 +0530

    ADD password reseting

--- Password Resetting.py
+++ Password Resetting.py
@@ -1,6 +1,5 @@
 pwd1 =input("Enter new password: ") 
 pwd2 =input("Re-enter new password: ")
-# TODO: Write the password logic using if..else conditional statements
 if pwd1 == pwd2:#quick comparison of both passwords
     print("Password Changed")#successful change message
 elif pwd1.lower() == pwd2.lower() and pwd1 != pwd2:#case sensitivity check
