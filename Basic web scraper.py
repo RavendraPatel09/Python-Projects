80a587a1412fc738a8e4bc1ff84883ebd0f91d90
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-21 23:51:40 +0530

    web scraper using python simple lib

---
+++ Basic web scraper.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+url = input("Enter URL: ")
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
+for link in soup.find_all("a"):
+    print(link.get("href"))
