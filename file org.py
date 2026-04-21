bec084958080f760ed0ad22e4c8f3e518d1b961e
patelsamerth9 <patelsamerth9@gmail.com>
2026-04-21 23:50:44 +0530

    file organize using python

---
+++ file org.py
@@ -0,0 +1,12 @@
+import os, shutil
+
+path = input("Enter folder path: ")
+
+for file in os.listdir(path):
+    ext = file.split(".")[-1]
+    folder = os.path.join(path, ext)
+
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+
+    shutil.move(os.path.join(path, file), os.path.join(folder, file))
